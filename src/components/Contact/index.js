77438497ca980085b017ad7514b1d9142c1652d7
tiannaj23 <tiannaj23@gmail.com>
2023-05-18 16:33:53 -0400

    working on contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_b18m4xg', 'template_iarhqyw', form.current, 'YOUR_P0NRLPvgc82aax1OBS')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   return (
-    <div>Contact</div>
+    <section>
+      <div className="contact-container">
+        <h2 className="--text-center">Contact</h2>
+        <form ref={form} onSubmit={sendEmail}className="--form-control--card--flex-center --dir-column">
+            <input type="text" placeholder='Name' name='user_name' required/>
+            <input type="email" placeholder='Email Address' name='user_email' required/>
+            <input type="text" placeholder='Subject' name='subject' required/>
+            <textarea name="message" cols="30" rows="10"></textarea>
+            <button className="--btn--btn-primary">Submit Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
