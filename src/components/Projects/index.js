93645790c2ff6f10edf0cd6b09c590d765a03474
tiannaj23 <tiannaj23@gmail.com>
2023-05-28 19:40:39 -0400

    added some project info to the  projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,28 +1,28 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function Projects() {
-//   const projectData = [
-//     {
-//       src: 'path/to/project1.png',
-//       description: 'Description of Project 1',
-//       siteUrl: 'https://example.com/project1',
-//       repoUrl: 'https://github.com/username/project1',
-//     },
-//     {
-//       src: 'path/to/project2.png',
-//       description: 'Description of Project 2',
-//       siteUrl: 'https://example.com/project2',
-//       repoUrl: 'https://github.com/username/project2',
-//     },
-//     // Add more projects here
-//   ];
+export default function Projects() {
+  const projectData = [
+    {
+      src: 'path/to/project1.png',
+      description: 'Welness Checkup',
+      siteUrl: 'https://dellmiles24.github.io/Wellness-Checkup/',
+      repoUrl: 'https://github.com/tiannaj23/Project-1',
+    },
+    {
+      src: 'path/to/project2.png',
+      description: 'Ideation',
+      siteUrl: 'https://sheltered-badlands-19260.herokuapp.com/',
+      repoUrl: 'https://github.com/tiannaj23/Project2',
+    },
+    // Add more projects here
+  ];
 
-//   return (
-//     // <div>
-//     //   <img src={src} alt="Project Screenshot" />
-//     //   <p>{description}</p>
-//     //   <a href={siteUrl} target="_blank" rel="noopener noreferrer">Deployed Site</a>
-//     //   <a href={repoUrl} target="_blank" rel="noopener noreferrer">Repository</a>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <img src={src} alt="Project Screenshot" />
+      <p>{description}</p>
+      <a href={siteUrl} target="_blank" rel="noopener noreferrer">Deployed Site</a>
+      <a href={repoUrl} target="_blank" rel="noopener noreferrer">Repository</a>
+    </div>
+  )
+}
